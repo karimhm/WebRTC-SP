0bce2c634dc089c0b4001e5d698b7589be29bd15
Karim H <karim@karhm.com>
2021-05-19 18:39:38 +0100

    Update Package.swift for WebRTC v 89.0.843830

---
+++ Package.swift
@@ -0,0 +1,21 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+    name: "GoogleMaps",
+    products: [
+        .library(
+            name: "WebRTC",
+            targets: [
+                "WebRTC"
+            ]
+        )
+    ],
+    targets: [
+        .binaryTarget(
+            name: "WebRTC",
+            url: "https://github.com/karimhm/WebRTC-SP/releases/download/89.0.843830/WebRTC.xcframework.zip",
+            checksum: "446f08092998e348be37663510df5c3c767095e0e555be335ebe24b519a7264a"
+        )
+    ]
+)
